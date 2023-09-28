4b63d27885b218a5862f5a0bfa78b367a5d004c2
mararo-ops <danielmararo1@gmail.com>
2023-09-28 23:14:18 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.activeElement('DOMContentLoaded', function() {
+    // Target the paragraph element with id="text".
+    const paragraph = document.getElementById('text');
+
+    // Replace the text with "This is really cool!".
+    paragraph.textContent = "This is really cool!";
+});
+console.log(textContent);
